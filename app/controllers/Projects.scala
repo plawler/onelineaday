a32b55f7053cb512a7f2194f8e2384f1bbe5a51e
plawler <paul.lawler@gmail.com>
2013-11-16 21:57:52 -0500

    doing this scala thing

---
+++ app/controllers/Projects.scala
@@ -0,0 +1,18 @@
+package controllers
+
+import play.api.mvc.{Action, Controller}
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: paullawler
+ * Date: 11/16/13
+ * Time: 9:50 PM
+ * To change this template use File | Settings | File Templates.
+ */
+object Projects extends Controller {
+
+  def index = Action {
+    Ok("Default page, list of current projects")
+  }
+
+}

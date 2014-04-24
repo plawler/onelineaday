fae74392b51353871fa7bdc24a05b5bef1b964f9
plawler <paul.lawler@gmail.com>
2014-04-24 08:48:08 -0400

    added delete and retire confirmation dialogs

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,6 +5,7 @@ import securesocial.core.SecureSocial
 
 object Application extends Controller with SecureSocial {
 
+  // todo: this is why the login page always flashes the error message "you must be logged in"
   def index = SecuredAction {
     Redirect(routes.Projects.projects)
   }
